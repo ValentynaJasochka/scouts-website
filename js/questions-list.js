37f560e0102dfef7c6d0ccde5c90d65c72cc52a6
Max <maxym.marchenko@gmail.com>
2024-03-11 22:21:13 +0200

    donation script

--- js/questions-list.js
+++ js/questions-list.js
@@ -1,38 +1,41 @@
 (() => {
   const body = document.querySelector('body');
   body.addEventListener('click', handleAnswer);
-  const refs = {
-    openAnswer: document.querySelector("[open-question]"),   
-    closeAnswer: document.querySelector("[close-question]"),
-    answer: document.querySelector("[answer-data]"),
-    
-  };
 
-  // refs.openAnswer.addEventListener("click", handleOpenAnswer); 
-  // refs.closeAnswer.addEventListener("click", handleCloseAnswer);
-function handleAnswer (e) {
-  let target = e.target;
-  console.log(target);
-  if (target.classList.contains('open-btn')) {
-    handleOpenAnswer()
+  function handleAnswer(e) {
+    let target = e.target;
+    if (
+      target.hasAttribute('data-open-close-question') && target.classList.contains("open")
+      || target.hasAttribute('data-open-question')
+    ) {
+
+      while (target) {
+        if (target.hasAttribute('data-question-answer')) {
+          target.querySelector("[data-open-close-question]").classList.remove("open");
+          target.querySelector("[data-open-close-question]").classList.add("close");
+
+          target.querySelectorAll("[data-answer-data]")
+            .forEach((el)=> el.style.display = "block");
+        }
+        target = target.parentElement;
+      }
+
+      return;
+    }
+    if (
+      target.hasAttribute('data-open-close-question') && target.classList.contains("close")
+
+    ) {
+      while (target) {
+        if (target.hasAttribute('data-question-answer')) {
+          target.querySelector("[data-open-close-question]").classList.remove("close");
+          target.querySelector("[data-open-close-question]").classList.add("open");
+
+          target.querySelectorAll("[data-answer-data]")
+            .forEach((el)=> el.style.display = "none");
+        }
+        target = target.parentElement;
+      }
+    }
   }
-  if (target.classList.contains('close-btn')) {
-    handleCloseAnswer()
-  }
-}
-
-
-  function handleOpenAnswer() {
-    // console.log("hjoho");
-    refs.answer.classList.remove("is-hidden");
-    refs.openAnswer.classList.add("is-hidden");
-    refs.closeAnswer.classList.remove("is-hidden");
-
-  }
-  function handleCloseAnswer() {
-    refs.answer.classList.add("is-hidden");
-    refs.openAnswer.classList.remove("is-hidden");
-    refs.closeAnswer.classList.add("is-hidden");
-  }
-})()
-
+})();
