e8ceba4b0ebefb241dc6662baa98ca8c2a90dec2
Max <maxym.marchenko@gmail.com>
2024-03-14 22:48:07 +0200

    question answer

--- js/questions-list.js
+++ js/questions-list.js
@@ -3,39 +3,22 @@
   body.addEventListener('click', handleAnswer);
 
   function handleAnswer(e) {
-    let target = e.target;
-    if (
-      target.hasAttribute('data-open-close-question') && target.classList.contains("open")
-      || target.hasAttribute('data-open-question')
-    ) {
-
-      while (target) {
-        if (target.hasAttribute('data-question-answer')) {
-          target.querySelector("[data-open-close-question]").classList.remove("open");
-          target.querySelector("[data-open-close-question]").classList.add("close");
-
-          target.querySelectorAll("[data-answer-data]")
-            .forEach((el)=> el.style.display = "block");
+    let toggler = e.target;
+    while (toggler) {
+      if (toggler.hasAttribute('data-open-close-question')) {
+        let container = toggler;
+        while (container) {
+          if (container.hasAttribute('data-question-answer')) {
+            if (container.classList.contains("opened")) {
+              container.classList.remove('opened');
+            } else {
+              container.classList.add('opened');
+            }
+          }
+          container = container.parentElement;
         }
-        target = target.parentElement;
       }
-
-      return;
-    }
-    if (
-      target.hasAttribute('data-open-close-question') && target.classList.contains("close")
-
-    ) {
-      while (target) {
-        if (target.hasAttribute('data-question-answer')) {
-          target.querySelector("[data-open-close-question]").classList.remove("close");
-          target.querySelector("[data-open-close-question]").classList.add("open");
-
-          target.querySelectorAll("[data-answer-data]")
-            .forEach((el)=> el.style.display = "none");
-        }
-        target = target.parentElement;
-      }
+      toggler = toggler.parentElement;
     }
   }
 })();
