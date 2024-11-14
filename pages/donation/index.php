9b9db372b0df3545e4a920957b7520ab047cf79f
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2024-11-14 18:37:51 +0200

    our-team

--- pages/donation/index.php
+++ pages/donation/index.php
@@ -1,246 +1,119 @@
 <?php
-
+$textsQuestions = include "texts-questions.inc";
+$textsLaw = include "texts-scouts-law.inc";
 ?>
- <main>
-      <div class="main-group">
-        <div class="container regular-section">
-          <div class="main-title-container">
-            <h1 class="main-title"><?= getLangText($texts["main-title"]) ?></h1>
-          </div>
-          <div class="donation-section">
-            <div class="donation">
-              <p class="donation-additional-text first-additional-text">
-              <?= getLangText($texts["first-additional-text"]) ?>
-              </p>
-              <div class="donation-block-wrapper first-donation-block-wrapper">
-                <div class="donation-block-img-wrapper first-img-wrapper">
-                  <img
-                    class="donation-block-points-img"
-                    src="./images/donation/photo1.jpg"
-                    alt=""
-                  />
-                </div>
-                <div class="side-wrapper">
-                  <div class="donation-block-text first-wrapper">
-                    <p class="donation-additional-text second-additional-text">
-                    <?= getLangText($texts["first-additional-text"]) ?>
-                    </p>
-                    <p class="donation-block-text-part">
-                    <?= getLangText($texts["second-additional-text"]) ?>
-                    </p>
-                    <p class="donation-block-text-part">
-                    <?= getLangText($texts["third-additional-text"]) ?>
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="donation-block-wrapper">
-                <div class="donation-block-img-wrapper second-img-wrapper">
-                  <img
-                    class="donation-block-points-img"
-                    src="./images/donation/photo2.jpg"
-                    alt=""
-                  />
-                </div>
-                <div class="side-wrapper">
-                  <div class="donation-block-text">
-                    <p class="donation-block-text-part">
-                    <?= getLangText($texts["donation-text"]) ?>
-                    </p>
-                  </div>
-                  <ul class="scouts-law-list">
-                    <li class="scouts-law-item">
-                      <div class="tik-btn">
-                        <img
-                          class="tik-btn"
-                          src="./images/scouting-method/icons/tik-btn.png"
-                          alt=""
-                        />
-                      </div>
-                      <p class="main-text law-text-item">
-                        Розвиток Скаутських груп у деокупованих територіях
-                        України та в малих містах.
-                      </p>
-                    </li>
-                    <li class="scouts-law-item">
-                      <div class="tik-btn">
-                        <img
-                          class="tik-btn"
-                          src="./images/scouting-method/icons/tik-btn.png"
-                          alt=""
-                        />
-                      </div>
-                      <p class="main-text law-text-item">
-                        Підготовку дорослих лідерів для їхньої ефективної роботи
-                        з дітьми у Скаутах України.
-                      </p>
-                    </li>
-                    <li class="scouts-law-item">
-                      <div class="tik-btn">
-                        <img
-                          class="tik-btn"
-                          src="./images/scouting-method/icons/tik-btn.png"
-                          alt=""
-                        />
-                      </div>
-                      <p class="main-text law-text-item">
-                        Підтримку дітей, які змушені були змінити місце
-                        проживання через війну.
-                      </p>
-                    </li>
-                    <li class="scouts-law-item">
-                      <div class="tik-btn">
-                        <img
-                          class="tik-btn"
-                          src="./images/scouting-method/icons/tik-btn.png"
-                          alt=""
-                        />
-                      </div>
-                      <p class="main-text law-text-item">
-                        Організацію адаптаційних Скаутських таборів для дітей,
-                        постраждалих від війни.
-                      </p>
-                    </li>
-                  </ul>
-                </div>
-                
-              </div>
-              <div class="donation-additional-block-wrapper" >
-                <div class="donation-block-text">
-                  <p class="donation-block-text-part">
-                  <?= getLangText($texts["donation-text-second"]) ?>
-                  </p>
-                </div>
-                <div class="donate-btn">
-                  <a href="" class="donate-link" target="_blank"
-                    ><?= getLangText($texts["donate-btn"]) ?></a
-                  >
-                </div>
-                <div class="donation-block-text">
-                  <p class="donation-block-text-part">
-                  <?= getLangText($texts["donation-text-third"]) ?>
-                  </p>
-                </div>
+<main>
+  <div class="main-group">
+    <div class="container regular-section">
+      <div class="main-title-container">
+        <h1 class="main-title"><?= getLangText($texts["main-title"]) ?></h1>
+      </div>
+      <div class="donation-section">
+        <div class="donation">
+          <p class="donation-additional-text first-additional-text">
+            <?= getLangText($texts["first-additional-text"]) ?>
+          </p>
+          <div class="donation-block-wrapper first-donation-block-wrapper">
+            <div class="donation-block-img-wrapper first-img-wrapper">
+              <img
+                class="donation-block-points-img"
+                src="./images/donation/photo1.jpg"
+                alt="" />
+            </div>
+            <div class="side-wrapper">
+              <div class="donation-block-text first-wrapper">
+                <p class="donation-additional-text second-additional-text">
+                  <?= getLangText($texts["first-additional-text"]) ?>
+                </p>
+                <p class="donation-block-text-part">
+                  <?= getLangText($texts["second-additional-text"]) ?>
+                </p>
+                <p class="donation-block-text-part">
+                  <?= getLangText($texts["third-additional-text"]) ?>
+                </p>
               </div>
             </div>
-            <div class="questions">
-              <p class="questions-title">Часті запитання:</p>
-              <ul class="questions-list">
-                <li class="questions-list-item" data-question-answer>
-                  <div class="questions-header-wrapper">
-                    <p class="questions-header" data-open-close-question>
-                      ЩЕ НЕ ПІЗНО ДОПОМАГАТИ?
+          </div>
+          <div class="donation-block-wrapper">
+            <div class="donation-block-img-wrapper second-img-wrapper">
+              <img
+                class="donation-block-points-img"
+                src="./images/donation/photo2.jpg"
+                alt="" />
+            </div>
+            <div class="side-wrapper">
+              <div class="donation-block-text">
+                <p class="donation-block-text-part">
+                  <?= getLangText($texts["donation-text"]) ?>
+                </p>
+              </div>
+              <ul class="scouts-law-list">
+                <?php
+                foreach ($textsLaw as $law) {
+                ?>
+
+                  <li class="scouts-law-item">
+                    <div class="tik-btn">
+                      <img
+                        class="tik-btn"
+                        src="./images/scouting-method/icons/tik-btn.png"
+                        alt="" />
+                    </div>
+                    <p class="main-text law-text-item">
+                      <?= getLangText($law['law-text']) ?>
                     </p>
-                    <div
-                      class="questions-header-open-close-btn"
-                      data-open-close-question
-                    ></div>
-                  </div>
-                  <div class="answer-wrapper">
-                    <p class="answer">
-                      Ніколи не пізно допомагати. Наші проєкти та ініціативи
-                      постійно потребують підтримки, і кожен внесок має значення
-                      для продовження нашої діяльності. Скаутинг продовжується
-                      незважаючи на усі виклики, ми розвиваємося та зростаємо,
-                      але в умовах війни, і наразі у нас постійно є Скаутські
-                      групи, які мусять адаптуватися до нових умов, переїжджати
-                      та ставити свою діяльність на паузу. Постійна підтримка
-                      дітей та молодих людей, особливо у такий складний час, є
-                      критично важливою для нас, аби продовжувати відкривати для
-                      них світ Скаутингу.
-                    </p>
-                  </div>
-                </li>
-                <li class="questions-list-item" data-question-answer>
-                  <div class="questions-header-wrapper">
-                    <p class="questions-header" data-open-close-question>
-                      ЯК МОЯ ПОЖЕРТВА МОЖЕ БУТИ ВИКОРИСТАНА?
-                    </p>
-                    <div
-                      class="questions-header-open-close-btn"
-                      data-open-close-question
-                    ></div>
-                  </div>
-                  <div class="answer-wrapper">
-                    <p class="answer">
-                      Ваші кошти можуть будуть спрямовані на організаційний
-                      розвиток, створення нових Скаутських осередків, навчання
-                      дорослих лідерів, виготовлення Скаутської форми, участь
-                      Скаутів в міжнародних заходах, літніх таборах у безпечних
-                      місцях тощо. Окрім цього, ми продовжуємо проводити
-                      гуманітарні та соціальні проєкти, такі як допомога
-                      переселенцям та інші ініціативи, спрямовані на підтримку
-                      громади.
-                    </p>
-                  </div>
-                </li>
-                <li class="questions-list-item" data-question-answer>
-                  <div class="questions-header-wrapper">
-                    <p class="questions-header" data-open-close-question>
-                      ЧИ МОЖНА ВАМ ДОПОМОГТИ ВІД ІМЕНІ КОМПАНІЇ?
-                    </p>
-                    <div
-                      class="questions-header-open-close-btn"
-                      data-open-close-question
-                    ></div>
-                  </div>
-                  <div class="answer-wrapper">
-                    <p class="answer">
-                      Так, ми завжди відкриті до співпраці та вдячні за будь-яку
-                      підтримку від компаній, які поділяють наші цінності та
-                      бажають долучитися до нашої місії. Партнерство із Скаутами
-                      України дозволяє створити якісне середовище для розвитку
-                      молодих людей, особливо у такий важкий час. Якщо ваша
-                      компанія бажає стати частиною нашої спільноти та допомогти
-                      у реалізації важливих ініціатив, будь ласка, зв'яжіться з
-                      нами.
-                    </p>
-                  </div>
-                </li>
-                <li class="questions-list-item" data-question-answer>
-                  <div class="questions-header-wrapper">
-                    <p class="questions-header" data-open-close-question>
-                      ЧИ Є МОЖЛИВІСТЬ ВИЗНАЧИТИ КОНКРЕТНИЙ ПРОЄКТ ДЛЯ МОЄЇ
-                      ПОЖЕРТВИ?
-                    </p>
-                    <div
-                      class="questions-header-open-close-btn"
-                      data-open-close-question
-                    ></div>
-                  </div>
-                  <div class="answer-wrapper">
-                    <p class="answer">
-                      Так, ви маєте можливість вказати конкретний проєкт, на
-                      який ви хотіли б спрямувати вашу пожертву. Після
-                      здійснення пожертви ми залишимося на зв'язку з вами, щоб
-                      інформувати про прогрес та вплив вашого внеску на обраний
-                      проєкт.
-                    </p>
-                  </div>
-                </li>
-                <li class="questions-list-item" data-question-answer>
-                  <div class="questions-header-wrapper">
-                    <p class="questions-header" data-open-close-question>
-                      ЧИ МОЖУ Я ДОПОМОГТИ НЕ ТІЛЬКИ ФІНАНСОВО, АЛЕ Й
-                      ВОЛОНТЕРСТВОМ?
-                    </p>
-                    <div
-                      class="questions-header-open-close-btn"
-                      data-open-close-question
-                    ></div>
-                  </div>
-                  <div class="answer-wrapper">
-                    <p class="answer">
-                      Так, ваша допомога може бути не лише фінансовою. Ми
-                      вітаємо всіх, хто хоче долучитися до нашої діяльності як
-                      волонтер. Відвідайте нашу сторінку про те, як приєднатися
-                      до нас у ролі <a href="./join-us#scout-leader">Скаут-лідера</a>.
-                    </p>
-                  </div>
-                </li>
+                  </li>
+                <?php
+                }
+                ?>
               </ul>
+            </div>
+
+          </div>
+          <div class="donation-additional-block-wrapper">
+            <div class="donation-block-text">
+              <p class="donation-block-text-part">
+                <?= getLangText($texts["donation-text-second"]) ?>
+              </p>
+            </div>
+            <div class="donate-btn">
+              <a href="" class="donate-link" target="_blank"><?= getLangText($texts["donate-btn"]) ?></a>
+            </div>
+            <div class="donation-block-text">
+              <p class="donation-block-text-part">
+                <?= getLangText($texts["donation-text-third"]) ?>
+              </p>
             </div>
           </div>
         </div>
+        <div class="questions">
+          <p class="questions-title"><?= getLangText($texts["questions-title"]) ?></p>
+          <ul class="questions-list">
+            <?php
+            foreach ($textsQuestions as $question) {
+            ?>
+
+              <li class="questions-list-item" data-question-answer>
+                <div class="questions-header-wrapper">
+                  <p class="questions-header" data-open-close-question>
+                    <?= getLangText($question['questions']) ?>
+                  </p>
+                  <div
+                    class="questions-header-open-close-btn"
+                    data-open-close-question></div>
+                </div>
+                <div class="answer-wrapper">
+                  <p class="answer">
+                    <?= getLangText($question['answer']) ?>
+                  </p>
+                </div>
+              </li>
+            <?php
+            }
+            ?>
+          </ul>
+        </div>
       </div>
-    </main>
+    </div>
+  </div>
+</main>
